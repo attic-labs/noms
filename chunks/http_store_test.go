119ba04a1dd79c7e58ff79892c7dc8d91852f2a4
Rafael Weinstein <rafael@atticlabs.io>
2015-08-28 10:35:32 -0700

    Fully shutdown server

--- chunks/http_store_test.go
+++ chunks/http_store_test.go
@@ -1,9 +1,11 @@
 package chunks
 
 import (
+	"net/http"
 	"testing"
 
 	"github.com/attic-labs/noms/Godeps/_workspace/src/github.com/stretchr/testify/suite"
+	"github.com/attic-labs/noms/d"
 )
 
 func TestHttpStoreTestSuite(t *testing.T) {
@@ -23,4 +25,10 @@ func (suite *HttpStoreTestSuite) SetupTest() {
 
 func (suite *HttpStoreTestSuite) TearDownTest() {
 	suite.server.Stop()
+
+	// Stop will have closed it's side of an existing KeepAlive socket. The next request will fail.
+	req, err := http.NewRequest("GET", "http://localhost:8000", nil)
+	d.Chk.NoError(err)
+	_, err = http.DefaultClient.Do(req)
+	d.Chk.Error(err)
 }
