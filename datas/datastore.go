e97a483985a779ac7a2c4e5541ceefdba55822a6
Rafael Weinstein <rafael@atticlabs.io>
2015-09-18 12:34:58 -0700

    Pull()

--- datas/datastore.go
+++ datas/datastore.go
@@ -6,6 +6,7 @@ import (
 	"github.com/attic-labs/noms/http"
 	"github.com/attic-labs/noms/ref"
 	"github.com/attic-labs/noms/types"
+	"github.com/attic-labs/noms/walk"
 )
 
 // DataStore provides versioned storage for noms values. Each DataStore instance represents one moment in history. Heads() returns the Commit from each active fork at that moment. The Commit() method returns a new DataStore, representing a new moment in history.
@@ -110,6 +111,53 @@ func getAncestors(commits SetOfCommit) SetOfCommit {
 		return
 	})
 	return ancestors
+}
+
+// Copys all chunks reachable from (and including) |r| but excluding all chunks reachable from (and including) |exclude| in |source| to |sink|.
+func (ds DataStore) CopyReachableChunksP(r, exclude ref.Ref, sink chunks.ChunkSink, concurrency int) {
+	excludeRefs := map[ref.Ref]bool{}
+	hasRef := func(r ref.Ref) bool {
+		return excludeRefs[r]
+	}
+
+	if exclude != (ref.Ref{}) {
+		refChan := make(chan ref.Ref, 1024)
+		addRef := func(r ref.Ref) {
+			refChan <- r
+		}
+
+		go func() {
+			walk.AllP(exclude, ds, addRef, concurrency)
+			close(refChan)
+		}()
+
+		for r := range refChan {
+			excludeRefs[r] = true
+		}
+	}
+
+	tcs := &teeChunkSource{ds, sink}
+	walk.SomeP(r, tcs, hasRef, concurrency)
+}
+
+// teeChunkSource just serves the purpose of writing to |sink| every chunk that is read from |source|.
+type teeChunkSource struct {
+	source chunks.ChunkSource
+	sink   chunks.ChunkSink
+}
+
+func (trs *teeChunkSource) Get(ref ref.Ref) chunks.Chunk {
+	c := trs.source.Get(ref)
+	if c == nil {
+		return nil
+	}
+
+	trs.sink.Put(c)
+	return c
+}
+
+func (trs *teeChunkSource) Has(ref ref.Ref) bool {
+	return trs.source.Has(ref)
 }
 
 type Flags struct {
