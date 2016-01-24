2057cef9a924ea3d2811f43e7fc4ce91fecdb16d
Rafael Weinstein <rafael@atticlabs.io>
2016-01-23 16:29:35 -0800

    wip: commit

--- js/src/datastore.js
+++ js/src/datastore.js
@@ -3,12 +3,24 @@
 import Chunk from './chunk.js';
 import Ref from './ref.js';
 import type {ChunkStore} from './chunk_store.js';
+import Struct from './struct.js';
+import {Package, registerPackage} from './package.js';
+import {Kind} from './noms_kind.js';
+import {Field, makeCompoundType, makePrimitiveType, makeStructType, makeType} from './type.js';
+import type {valueOrPrimitive} from './value.js';
+import {NomsMap, MapLeafSequence} from './map.js';
+import {NomsSet, SetLeafSequence} from './map.js';
+import Ref from './ref.js';
+import {readValue} from './readValue.js';
+import {writeValue} from './encode.js';
 
-export default class DataStore {
+export class DataStore {
   _cs: ChunkStore;
+  datasets: Promise<NomsMap<string, Ref>>;
 
   constructor(cs: ChunkStore) {
     this._cs = cs;
+    this.datasets = this._datasetsFromRootRef();
   }
 
   async getRoot(): Promise<Ref> {
@@ -32,4 +44,67 @@ export default class DataStore {
   }
 
   close() {}
+
+  _datasetsFromRootRef(): Promise<NomsMap<string, Ref>> {
+    return this.getRoot().then(
+      rootRef => rootRef ? readValue(rootRef, this._cs) : newEmptyCommitMap());
+  }
+
+  head(datasetID: string): Promise<?Commit> {
+    return this.datasets.then(datasets => datasets.get(datasetID).then(
+      commitRef => commitRef ? readValue(commitRef, this._cs) : null));
+  }
+
+  async commit(datasetID: string, commit: Commit): Promise<DataStore> {
+    const currentRootRef = await this.getRoot();
+    let currentDatasets: NomsMap<string, Ref> = await this.datasets;
+    if (!currentRootRef.empty && !currentDatasets.ref.equal(currentRootRef)) {
+      currentDatasets = await this._datasetsFromRootRef();
+    }
+
+    if (!currentRootRef.empty) {
+      const currentHeadRef = await currentDatasets.get(datasetID);
+      if (currentHeadRef) {
+        if (commitRef.equals(currentHeadRef)) {
+          return this;
+        }
+
+        if (await this.descendsFrom(commit, currentHeadRef)) {
+          throw new MergeNeededError(this);
+        }
+      }
+    }
+
+    // TODO: This Commit will be orphaned if the UpdateRoot below fails
+    const commitRef = writeValue(commit, commit.type, this);
+
+
+  }
+}
+
+const datasPackage = new Package([makeStructType('S1', [
+  new Field('value', makePrimitiveType(Kind.Value), false),
+  new Field('parents',
+              makeCompoundType(Kind.Set, makeCompoundType(Kind.Ref, makeType(new Ref(), 0))), true),
+], [])], []);
+const packageRef = datasPackage.ref;
+const commitType = makeType(packageRef, 0);
+const commitTypeDef = datasPackage.types[0];
+const commitMapType = makeCompoundType(Kind.Map, makePrimitiveType(Kind.String),
+                                       makeCompoundType(Kind.Ref, commitType));
+const commitSetType = makeCompoundType(Kind.Set, makeCompoundType(Kind.Ref, commitType));
+registerPackage(datasPackage);
+
+function newEmptyCommitMap(cs: ChunkStore): NomsMap<string, Ref> {
+  return new NomsMap(cs, commitMapType, new MapLeafSequence(commitMapType, []));
+}
+
+function newParentSet(cs: ChunkStore, commitRefs: Array<Ref>): NomsSet<Ref> {
+  return new NomsSet(cs, commitSetType, new SetLeafSequence(commitSetType, commitRefs));
+}
+
+export class Commit extends Struct {
+  constructor(cs: ChunkStore, parents: Array<Ref>, value: valueOrPrimitive) {
+    super(commitType, commitTypeDef, {parents: newParentSet(cs, parents), value: value});
+  }
 }
--- js/src/datastore_test.js
+++ js/src/datastore_test.js
@@ -4,7 +4,7 @@ import {suite, test} from 'mocha';
 import {assert} from 'chai';
 import Chunk from './chunk.js';
 import MemoryStore from './memory_store.js';
-import DataStore from './datastore.js';
+import {Commit, DataStore} from './datastore.js';
 
 suite('DataStore', () => {
   test('access', async () => {
@@ -25,5 +25,67 @@ suite('DataStore', () => {
 
     has = await ds.has(c.ref);
     assert.isTrue(has);
+  });
+
+  test('commit', async () => {
+    const ms = new MemoryStore();
+    const ds = new DataStore(ms);
+
+    const datasets = ds.datasets;
+    assert.strictEqual(0, datasets.size);
+
+    // |a|
+    const aCommit = new Commit([], 'a');
+
+
+    const ds2, err := ds.Commit(datasetID, aCommit)
+    assert.NoError(err)
+
+    // The old datastore still still has no head.
+    _, ok := ds.MaybeHead(datasetID)
+    assert.False(ok)
+
+    // The new datastore has |a|.
+    aCommit1 := ds2.Head(datasetID)
+    assert.True(aCommit1.Value().Equals(a))
+    ds = ds2
+
+    // |a| <- |b|
+    b := types.NewString("b")
+    bCommit := NewCommit(cs).SetValue(b).SetParents(NewSetOfRefOfCommit(cs).Insert(NewRefOfCommit(aCommit.Ref())))
+    ds, err = ds.Commit(datasetID, bCommit)
+    assert.NoError(err)
+    assert.True(ds.Head(datasetID).Value().Equals(b))
+
+    // |a| <- |b|
+    //   \----|c|
+    // Should be disallowed.
+    c := types.NewString("c")
+    cCommit := NewCommit(cs).SetValue(c)
+    ds, err = ds.Commit(datasetID, cCommit)
+    assert.Error(err)
+    assert.True(ds.Head(datasetID).Value().Equals(b))
+
+    // |a| <- |b| <- |d|
+    d := types.NewString("d")
+    dCommit := NewCommit(cs).SetValue(d).SetParents(NewSetOfRefOfCommit(cs).Insert(NewRefOfCommit(bCommit.Ref())))
+    ds, err = ds.Commit(datasetID, dCommit)
+    assert.NoError(err)
+    assert.True(ds.Head(datasetID).Value().Equals(d))
+
+    // Attempt to recommit |b| with |a| as parent.
+    // Should be disallowed.
+    ds, err = ds.Commit(datasetID, bCommit)
+    assert.Error(err)
+    assert.True(ds.Head(datasetID).Value().Equals(d))
+
+    // Add a commit to a different datasetId
+    _, err = ds.Commit("otherDs", aCommit)
+    assert.NoError(err)
+
+    // Get a fresh datastore, and verify that both datasets are present
+    newDs := NewDataStore(cs)
+    datasets2 := newDs.Datasets()
+    assert.Equal(uint64(2), datasets2.Len())
   });
 });
