9afef61da9bebfbe8107bced3d7f46eeb8a001fd
Erik Arvidsson <erik.arvidsson@gmail.com>
2016-04-08 13:56:30 -0700

    More cleanup

--- js/src/compare.js
+++ js/src/compare.js
@@ -9,24 +9,17 @@ import type {Type} from './type.js';
 
 export function less(v1: any, v2: any): boolean {
   invariant(v1 !== null && v1 !== undefined && v2 !== null && v2 !== undefined);
+  invariant(typeof v1 === typeof v2);
 
   if (typeof v1 === 'object') {
-    invariant(typeof v2 === 'object');
     return (v1:Value).less(v2);
   }
 
-  if (typeof v1 === 'string') {
-    invariant(typeof v2 === 'string');
-    return v1 < v2;
-  }
-
   if (typeof v1 === 'boolean') {
-    invariant(typeof v2 === 'boolean');
     return compareBools(v1, v2) === -1;
   }
 
-  invariant(typeof v1 === 'number');
-  invariant(typeof v2 === 'number');
+  invariant(typeof v1 === 'number' || typeof v1 === 'string');
   return v1 < v2;
 }
 
@@ -36,13 +29,13 @@ export function equals(v1: valueOrPrimitive, v2: valueOrPrimitive): boolean {
   }
 
   invariant(v1 !== null && v1 !== undefined && v2 !== null && v2 !== undefined);
+  invariant(typeof v1 === typeof v2);
 
   if (typeof v1 === 'object') {
-    invariant(typeof v2 === 'object');
-    return (v1: Value).equals((v2: Value));
+    return v1.equals(v2);
   }
+
   invariant(typeof v1 === 'string' || typeof v1 === 'number' || typeof v1 === 'boolean');
-  invariant(typeof v1 === typeof v2);
   return v1 === v2;
 }
 
