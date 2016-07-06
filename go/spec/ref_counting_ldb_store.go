2325f46ac5bdfae3effd807f67d035de417613f1
Benjamin Kalman <kalman@attic.io>
2016-07-06 14:29:16 -0700

    Simpler using interface embedding

--- go/spec/ref_counting_ldb_store.go
+++ go/spec/ref_counting_ldb_store.go
@@ -7,45 +7,16 @@ package spec
 import (
 	"github.com/attic-labs/noms/go/chunks"
 	"github.com/attic-labs/noms/go/d"
-	"github.com/attic-labs/noms/go/hash"
 )
 
 type refCountingLdbStore struct {
-	store    *chunks.LevelDBStore
+	*chunks.LevelDBStore
 	refCount int
 	closeFn  func()
 }
 
 func newRefCountingLdbStore(path string, closeFn func()) *refCountingLdbStore {
 	return &refCountingLdbStore{chunks.NewLevelDBStoreUseFlags(path, ""), 1, closeFn}
-}
-
-func (r *refCountingLdbStore) Get(h hash.Hash) chunks.Chunk {
-	return r.store.Get(h)
-}
-
-func (r *refCountingLdbStore) Has(h hash.Hash) bool {
-	return r.store.Has(h)
-}
-
-func (r *refCountingLdbStore) Version() string {
-	return r.store.Version()
-}
-
-func (r *refCountingLdbStore) Put(c chunks.Chunk) {
-	r.store.Put(c)
-}
-
-func (r *refCountingLdbStore) PutMany(chunks []chunks.Chunk) chunks.BackpressureError {
-	return r.store.PutMany(chunks)
-}
-
-func (r *refCountingLdbStore) Root() hash.Hash {
-	return r.store.Root()
-}
-
-func (r *refCountingLdbStore) UpdateRoot(current, last hash.Hash) bool {
-	return r.store.UpdateRoot(current, last)
 }
 
 func (r *refCountingLdbStore) AddRef() {
@@ -56,7 +27,7 @@ func (r *refCountingLdbStore) Close() (err error) {
 	d.Chk.True(r.refCount > 0)
 	r.refCount--
 	if r.refCount == 0 {
-		err = r.store.Close()
+		err = r.LevelDBStore.Close()
 		r.closeFn()
 	}
 	return
