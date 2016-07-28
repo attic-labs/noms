643fd3a1c59e8af9fa3eb0469f6335dfc7769dfb
Rafael Weinstein <rafael@atticlabs.io>
2016-07-28 16:33:02 -0700

    Align searc impl js<->go

--- js/src/indexed-sequence.js
+++ js/src/indexed-sequence.js
@@ -43,7 +43,7 @@ export class IndexedSequence<T> extends Sequence<T> {
 
 export class IndexedSequenceCursor<T> extends SequenceCursor<T, IndexedSequence> {
   advanceToOffset(idx: number): number {
-    this.idx = search(this.length, (i: number) => this.sequence.getOffset(i) - idx);
+    this.idx = search(this.length, (i: number) => idx <= this.sequence.getOffset(i));
 
     if (this.sequence.isMeta && this.idx === this.length) {
       this.idx = this.length - 1;
