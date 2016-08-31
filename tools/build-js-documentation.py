75a20578ee7223b637cbcba9d43c47e69ead1380
Erik Arvidsson <erik.arvidsson@gmail.com>
2016-08-31 15:24:53 -0700

    Add node to the PATH for documentation build

--- tools/build-js-documentation.py
+++ tools/build-js-documentation.py
@@ -9,12 +9,19 @@ This script builds the JS SDK documentation and puts the generated files
 in $WORKSPACE/build.
 '''
 
-import os, subprocess, json
+import copy
+import json
+import os
+import subprocess
 
 def main():
     # Workspace is the root of the builder, e.g. "/var/lib/jenkins/workspace/builder".
     workspace = os.getenv('WORKSPACE')
     assert workspace
+
+    # Directory where node/npm binaries have been installed.
+    node_bin = '/var/lib/jenkins/node-v5.11.1-linux-x64/bin'
+    assert os.path.exists(node_bin)
 
     noms_dir = os.path.join(workspace, 'src/github.com/attic-labs/noms')
     noms_js_dir = os.path.join(noms_dir, 'js/noms')
@@ -23,8 +30,17 @@ def main():
 
     version = data['version']
 
+    # This directory does not really matter
     os.chdir(workspace)
-    subprocess.check_call(['npm', 'install', 'documentation'])
+
+    env = copy.copy(os.environ)
+    env.update({
+        'PATH': '%s:%s' % (os.getenv('PATH'), node_bin),
+    })
+
+    proc = subprocess.Popen(['npm', 'install', 'documentation'], env=env)
+    proc.wait()
+    assert proc.returncode == 0
 
     cmd = [
         os.path.join(workspace, 'node_modules', '.bin', 'documentation'), 'build',
@@ -37,7 +53,9 @@ def main():
         '--output', os.path.join(workspace, 'build', version),
         '--project-version', version
     ]
-    subprocess.check_call(cmd)
+    proc = subprocess.Popen(cmd, env=env)
+    proc.wait()
+    assert proc.returncode == 0
 
 if __name__ == '__main__':
     main()
