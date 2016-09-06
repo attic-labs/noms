cb6db9542d731dfb5898d49b50ca83b5d9a80675
Zane Starr <zane@Zanes-MacBook-Pro-2.local>
2016-09-06 13:54:02 -0700

    Rename comma to parsedDelimiter to be more clear

--- samples/go/csv/csv-import/importer.go
+++ samples/go/csv/csv-import/importer.go
@@ -114,7 +114,7 @@ func main() {
 		r = progressreader.New(r, getStatusPrinter(size))
 	}
 
-	comma, err := csv.StringToRune(*delimiter)
+	parsedDelimiter, err := csv.StringToRune(*delimiter)
 	d.CheckErrorNoUsage(err)
 
 	var dest int
@@ -133,7 +133,7 @@ func main() {
 		return
 	}
 
-	cr := csv.NewCSVReader(r, comma)
+	cr := csv.NewCSVReader(r, parsedDelimiter)
 	csv.SkipRecords(cr, *skipRecords)
 
 	var headers []string
