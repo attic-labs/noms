49a7cc04c4211ee3d0d6b2eb6d3f166777758a86
Chris Masone <cmasone@atticlabs.io>
2016-09-14 11:02:31 -0700

    Add FindCommonAncestor for Commits
    Once we integrate noms-merge into the `noms commit` command, this
    function will allow us to stop requiring users to pass in the common
    ancestor to be used when merging. The code can just find it and merge
    away.
    
    Toward #2535

--- go/datas/commit.go
+++ go/datas/commit.go
@@ -5,6 +5,8 @@
 package datas
 
 import (
+	"sort"
+
 	"github.com/attic-labs/noms/go/d"
 	"github.com/attic-labs/noms/go/types"
 )
@@ -64,6 +66,50 @@ func CommitDescendsFrom(commit types.Struct, ancestor types.Ref, vr types.ValueR
 		ancestors = getAncestors(ancestors, ancestor.Height(), vr)
 	}
 	return true
+}
+
+// FindCommonAncestor returns the most recent common ancestor of c1 and c2, if
+// one exists, setting ok to true. If there is no common ancestor, ok is set to false.
+func FindCommonAncestor(c1, c2 types.Struct, vr types.ValueReader) (a types.Struct, ok bool) {
+	c1Q, c2Q := &types.RefByHeight{types.NewRef(c1)}, &types.RefByHeight{types.NewRef(c2)}
+
+	makeSet := func(refs types.RefSlice) types.Set {
+		vals := make(types.ValueSlice, refs.Len())
+		for i, r := range refs {
+			vals[i] = r
+		}
+		return types.NewSet(vals...)
+	}
+
+	for !c1Q.Empty() && !c2Q.Empty() {
+		c1Ht, c2Ht := c1Q.MaxHeight(), c2Q.MaxHeight()
+		if c1Ht == c2Ht {
+			c1Parents, c2Parents := c1Q.PopRefsOfHeight(c1Ht), c2Q.PopRefsOfHeight(c2Ht)
+			intersection := types.NewIntersectionIterator(makeSet(c1Parents).Iterator(), makeSet(c2Parents).Iterator())
+			if common := intersection.Next(); common != nil {
+				return common.(types.Ref).TargetValue(vr).(types.Struct), true
+			}
+
+			parentsToQueue(c1Parents, c1Q, vr)
+			parentsToQueue(c2Parents, c2Q, vr)
+		} else if c1Ht > c2Ht {
+			parentsToQueue(c1Q.PopRefsOfHeight(c1Ht), c1Q, vr)
+		} else {
+			parentsToQueue(c2Q.PopRefsOfHeight(c2Ht), c2Q, vr)
+		}
+	}
+	return
+}
+
+func parentsToQueue(refs types.RefSlice, q *types.RefByHeight, vr types.ValueReader) {
+	for _, r := range refs {
+		c := r.TargetValue(vr).(types.Struct)
+		p := c.Get(ParentsField).(types.Set)
+		p.IterAll(func(v types.Value) {
+			q.PushBack(v.(types.Ref))
+		})
+	}
+	sort.Sort(q)
 }
 
 // getAncestors returns set of direct ancestors with height >= minHeight
