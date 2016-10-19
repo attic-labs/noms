beb3eb5b9939732ae842239c37536414f07d6d0b
Zane Starr <zane@Zanes-MacBook-Pro-2.local>
2016-10-19 14:18:22 -0700

    Refactors face rectangle to face center point

--- samples/js/fb/find-photos/src/main.js
+++ samples/js/fb/find-photos/src/main.js
@@ -97,7 +97,7 @@ async function main(): Promise<void> {
         photo.geoposition = getGeo(v);
       }
       if (isSubtype(tagsType, v.type)) {
-        photo.faces = await getFaces(v);
+        photo.faces_centered = await getFaces(v);
       }
       result = result.then(r => r.add(newStruct('Photo', photo)));
       return true;
@@ -142,11 +142,9 @@ async function getFaces(photo): Promise<Set<Struct>> {
   const mh = 0.33;
   const result = [];
   await photo.tags.data.forEach(v => {
-    const x = Math.max(0, v.x / 100 - mw / 2);
-    const y = Math.max(0, v.y / 100 - mh / 2);
-    const w = Math.min(mw, 1 - x);
-    const h = Math.min(mh, 1 - y);
-    result.push(newStruct('', {x, y, w, h, name: v.name}));
+    const x =  v.x / 100;
+    const y = v.y / 100;
+    result.push(newStruct('', {x, y, name: v.name}));
   });
   return new Set(result);
 }
