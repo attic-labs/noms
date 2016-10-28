b95300ad729b49c73280e5a86cbbb3e29c62026f
Erik Arvidsson <erik.arvidsson@gmail.com>
2016-10-28 14:07:55 -0700

    testing

--- tools/run_pr_builder.sh
+++ tools/run_pr_builder.sh
@@ -11,8 +11,8 @@ set -eux
 export GOPATH=${WORKSPACE}
 NOMS_DIR=${WORKSPACE}/src/github.com/attic-labs/noms
 
-go version
 node --version
+go version
 
 # go list is expensive, only do it once.
 GO_LIST="$(go list ./... | grep -v /vendor/ | grep -v /samples/js/)"
