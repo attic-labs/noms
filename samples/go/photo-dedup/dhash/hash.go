71f948646d1cc37b9ddac9f6d49af5e3ee69a9d7
Eric Halpern <ehalpern@attic.io>
2016-10-31 14:24:27 -0700

    Derive dhash from https://github.com/andybalholm/dhash

--- samples/go/photo-dedup/dhash/hash.go
+++ samples/go/photo-dedup/dhash/hash.go
@@ -1,16 +1,15 @@
-// Derived from https://github.com/andybalholm/dhash
 package dhash
 
 import (
 	"fmt"
 	"image"
 	"strings"
+
+	"github.com/andybalholm/go-bit"
 )
 
 // A Hash is a 128-bit perceptual hash.
 type Hash [2]uint64
-
-var NilHash = Hash{}
 
 func (h Hash) String() string {
 	return fmt.Sprintf("%016x%016x", h[0], h[1])
@@ -87,32 +86,5 @@ func New(img image.Image) Hash {
 
 // Distance returns the number of bits different between two Hash values.
 func Distance(h1, h2 Hash) int {
-	return bitCount(h1[0]^h2[0]) + bitCount(h1[1]^h2[1])
-}
-
-// Count returns the number of nonzero bits in w.
-func bitCount(w uint64) int {
-	// “Software Optimization Guide for AMD64 Processors”, Section 8.6.
-	const maxw = 1<<64 - 1
-	const bpw = 64
-
-	// Compute the count for each 2-bit group.
-	// Example using 16-bit word w = 00,01,10,11,00,01,10,11
-	// w - (w>>1) & 01,01,01,01,01,01,01,01 = 00,01,01,10,00,01,01,10
-	w -= (w >> 1) & (maxw / 3)
-
-	// Add the count of adjacent 2-bit groups and store in 4-bit groups:
-	// w & 0011,0011,0011,0011 + w>>2 & 0011,0011,0011,0011 = 0001,0011,0001,0011
-	w = w&(maxw/15*3) + (w>>2)&(maxw/15*3)
-
-	// Add the count of adjacent 4-bit groups and store in 8-bit groups:
-	// (w + w>>4) & 00001111,00001111 = 00000100,00000100
-	w += w >> 4
-	w &= maxw / 255 * 15
-
-	// Add all 8-bit counts with a multiplication and a shift:
-	// (w * 00000001,00000001) >> 8 = 00001000
-	w *= maxw / 255
-	w >>= (bpw/8 - 1) * 8
-	return int(w)
+	return bit.Count(h1[0]^h2[0]) + bit.Count(h1[1]^h2[1])
 }
