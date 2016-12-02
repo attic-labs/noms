431bb5d4e91db5b29843c87d094763f5d991a39a
Eric Halpern <ehalpern@attic.io>
2016-12-01 18:24:32 -0800

    Support marshaling from and unmarshaling to *types.Type

--- go/marshal/decode.go
+++ go/marshal/decode.go
@@ -144,6 +144,12 @@ func typeDecoder(t reflect.Type, tags nomsTags) decoderFunc {
 			return mapFromSetDecoder(t)
 		}
 		return mapDecoder(t, tags)
+	case reflect.Ptr:
+		// Allow implementations of types.Value (like *types.Type)
+		if t.Implements(nomsValueInterface) {
+			return nomsValueDecoder
+		}
+		panic(&UnsupportedTypeError{Type: t})
 	default:
 		panic(&UnsupportedTypeError{Type: t})
 	}
