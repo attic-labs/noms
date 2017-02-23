c3ef59de100d62bb3447e6710baf8bacf5cb324e
Erik Arvidsson <erik.arvidsson@gmail.com>
2017-02-23 12:02:47 -0800

    Move code around

--- go/marshal/encode_type.go
+++ go/marshal/encode_type.go
@@ -46,3 +46,88 @@ func MustMarshalType(v interface{}) *types.Type {
 	d.Chk.NoError(err)
 	return t
 }
+
+var typeOfTypesType = reflect.TypeOf((*types.Type)(nil))
+
+type nomsTypeOptions struct {
+	IgnoreOmitempty bool
+}
+
+func nomsType(t reflect.Type, parentStructTypes []reflect.Type, tags nomsTags, options nomsTypeOptions) *types.Type {
+	if t.Implements(marshalerInterface) {
+		// There is no way to determine the noms type now, it can be different each
+		// time MarshalNoms is called. This is handled further up the stack.
+		return nil
+	}
+
+	if t.Implements(nomsValueInterface) {
+		if t == typeOfTypesType {
+			return types.TypeType
+		}
+
+		// Use Name because List and Blob are convertible to each other on Go.
+		switch t.Name() {
+		case "Blob":
+			return types.BlobType
+		case "Bool":
+			return types.BoolType
+		case "Number":
+			return types.NumberType
+		case "String":
+			return types.StringType
+		}
+		// The rest of the Noms types need the value to get the exact type.
+		return nil
+	}
+
+	switch t.Kind() {
+	case reflect.Bool:
+		return types.BoolType
+	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64, reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64, reflect.Float32, reflect.Float64:
+		return types.NumberType
+	case reflect.String:
+		return types.StringType
+	case reflect.Struct:
+		return structNomsType(t, parentStructTypes, options)
+	case reflect.Array, reflect.Slice:
+		elemType := nomsType(t.Elem(), parentStructTypes, nomsTags{}, options)
+		if elemType != nil {
+			return types.MakeListType(elemType)
+		}
+	case reflect.Map:
+		keyType := nomsType(t.Key(), parentStructTypes, nomsTags{}, options)
+		if keyType == nil {
+			break
+		}
+
+		if shouldMapEncodeAsSet(t, tags) {
+			return types.MakeSetType(keyType)
+		}
+
+		valueType := nomsType(t.Elem(), parentStructTypes, nomsTags{}, options)
+		if valueType != nil {
+			return types.MakeMapType(keyType, valueType)
+		}
+	}
+
+	// This will be reported as an error at a different layer.
+	return nil
+}
+
+// structNomsType returns the Noms types.Type if it can be determined from the
+// reflect.Type. In some cases we cannot determine the type by only looking at
+// the type but we also need to look at the value. In this cases this returns
+// nil and we have to wait until we have a value to be able to determine the
+// type.
+func structNomsType(t reflect.Type, parentStructTypes []reflect.Type, options nomsTypeOptions) *types.Type {
+	for i, pst := range parentStructTypes {
+		if pst == t {
+			return types.MakeCycleType(uint32(i))
+		}
+	}
+
+	parentStructTypes = append(parentStructTypes, t)
+
+	_, structType, _ := typeFields(t, parentStructTypes, options)
+	return structType
+}
