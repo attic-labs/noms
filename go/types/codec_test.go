c3f24282beefcc540fea6bafef78ef8d8ba15ffe
Erik Arvidsson <erik.arvidsson@gmail.com>
2017-04-10 12:09:39 -0700

    rebase and add comment

--- go/types/codec_test.go
+++ go/types/codec_test.go
@@ -17,18 +17,20 @@ func TestCodecWriteNumber(t *testing.T) {
 		assert.Equal(t, exp, w.data())
 	}
 
+	// We use zigzag encoding for the signed bit. For positive n we do 2*n and for negative we do 2*-n - 1
 	test(0, []byte{0, 0}) //  0 * 2 **  0
 
-	test(1, []byte{1 * 2, 0})           //  1 * 2 **  0
-	test(2, []byte{1 * 2, 1 * 2})       //  1 * 2 **  1
-	test(-2, []byte{1*2 - 1, 1 + 1})    // -1 * 2 **  1
-	test(.5, []byte{1 * 2, 1*2 - 1})    //  1 * 2 ** -1
-	test(-.5, []byte{1*2 - 1, 1*2 - 1}) // -1 * 2 ** -1
-	test(.25, []byte{1 * 2, 2*2 - 1})   //  1 * 2 ** -2
-	test(3, []byte{3 * 2, 0})           // 0b11 * 2 ** 0
+	test(1, []byte{1 * 2, 0})            //  1 * 2 **  0
+	test(2, []byte{1 * 2, 1 * 2})        //  1 * 2 **  1
+	test(-2, []byte{(1 * 2) - 1, 1 * 2}) // -1 * 2 **  1
+	test(.5, []byte{1 * 2, 1*2 - 1})     //  1 * 2 ** -1
+	test(-.5, []byte{1*2 - 1, 1*2 - 1})  // -1 * 2 ** -1
+	test(.25, []byte{1 * 2, 2*2 - 1})    //  1 * 2 ** -2
+	test(3, []byte{3 * 2, 0})            // 0b11 * 2 ** 0
 
 	test(15, []byte{15 * 2, 0})     // 0b1111 * 2**0
 	test(256, []byte{1 * 2, 8 * 2}) // 1 * 2*8
+	test(-15, []byte{15*2 - 1, 0})  // -15 * 2*0
 }
 
 func TestCodecReadNumber(t *testing.T) {
@@ -51,4 +53,5 @@ func TestCodecReadNumber(t *testing.T) {
 
 	test([]byte{15 * 2, 0}, 15)     // 0b1111 * 2**0
 	test([]byte{1 * 2, 8 * 2}, 256) // 1 * 2*8
+	test([]byte{15*2 - 1, 0}, -15)  // -15 * 2*0
 }
